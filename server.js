2acbae16bfc1577001676c208ffa9d106e48e359
Victor <victorjuez.alu@gmail.com>
2019-05-28 18:20:29 +0200

    Different environments set up

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+process.env.NODE_ENV = 'production';
+
+// config variables
+const config = require('./config.js');
+
 // create express app
 const app = express();
 
@@ -17,7 +22,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // Connecting to the database
-mongoose.connect(dbConfig.url, {
+mongoose.connect(global.gConfig.database, {
     useNewUrlParser: true
 }).then(() => {
     console.log("Successfully connected to the database");    
@@ -28,12 +33,14 @@ mongoose.connect(dbConfig.url, {
 
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+    //res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+    res.json(global.gConfig);
 });
 
 require('./app/routes/note.routes.js')(app);
 
 // listen for requests
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+app.listen(global.gConfig.node_port, () => {
+    //console.log("Server is listening on port 3000");
+    console.log(`${global.gConfig.app_name} listening on port ${global.gConfig.node_port}`);
 });
