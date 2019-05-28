0643c59dcbf3e1abd34e5acf8971e7839947e40e
Victor <victorjuez.alu@gmail.com>
2019-05-28 17:38:12 +0200

    Test server working succesfully (localhost)

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.get('/', (req, res) => {
     res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
 });
 
+require('./app/routes/note.routes.js')(app);
+
 // listen for requests
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
