dfc8b3e479790965d424b90d3a57229b6776c688
Victor <victorjuez.alu@gmail.com>
2019-06-21 18:46:55 +0200

    relative url + little fix

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const _ = require('lodash');
 
 // module variables
 const config = require('./config.json');
-const defaultConfig = config.production;
-const environment = process.env.NODE_ENV || 'production';
+const defaultConfig = config.development;
+const environment = process.env.NODE_ENV || 'development';
 const environmentConfig = config[environment];
 const finalConfig = _.merge(defaultConfig, environmentConfig);
 
