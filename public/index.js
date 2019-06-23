d2faa96ff1a0138b44374862f0651af59022371e
Julián <julianmangut@gmail.com>
2019-06-23 17:33:42 +0200

    login, register page done

---
+++ public/index.js
@@ -0,0 +1,55 @@
+$(document).ready(() => {
+
+
+
+});
+
+var optionSelected;
+
+function chargeForm(option) {
+
+    console.log(option);
+
+    optionSelected = option;
+
+    var form = ' <form> ' +
+        'Email:<br>' +
+        '<input id="email" type="text" name="email" value="example@example.com">' +
+        '<br>' +
+        'Password:<br>' +
+        '<input id="password" type="password" name="password" value="*****">' +
+        '<br><br>' +
+        '<input class="btn btn-info" type="button" value="Submit" onclick="submitData()">' +
+        '</form>'
+
+    $("#containerButtons").html(form);
+
+}
+
+function submitData() {
+
+    if (optionSelected == "register")
+        var url = "signUp";
+    else
+        var url = "signIn";
+
+    console.log(document.getElementById("email"));
+    console.log("/user/" + url);
+    $.ajax({
+        type: "POST",
+        url: '/user/' + url,
+        dataType : "json",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({
+            email: document.getElementById("email").value,
+            password: document.getElementById("password").value
+        }),
+        success: function (respond) {
+            window.location="main.html?email=" + document.getElementById("email").value;
+        },
+        error: function (respond) {
+            console.log("Error");
+            console.log(respond);
+        }
+    })
+}
--- public/main.js
+++ public/main.js
@@ -1,3 +1,13 @@
+$.urlParam = function (name) {
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results == null) {
+        return null;
+    } else {
+        return results[1] || 0;
+    }
+}
+
+
 $(document).ready(function () {
     var altura = $('.filter').offset().top;
 
